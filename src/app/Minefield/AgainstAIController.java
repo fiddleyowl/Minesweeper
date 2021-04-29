3a0ae1e65b34a3fc402a2a8b2978b9d04e7063e5
FallenLucifer5 <82928147+FallenLucifer5@users.noreply.github.com>
2021-04-29 19:16:42 +0800

    Robot.

---
+++ src/app/Minefield/AgainstAIController.java
@@ -0,0 +1,91 @@
+package app.Minefield;
+
+import SupportingFiles.Audio.Sound;
+import app.PublicDefinitions;
+import javafx.application.Platform;
+
+import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static app.PublicDefinitions.*;
+
+public class AgainstAIController extends MinefieldController {
+
+    //region Variables Declaration
+
+    boolean isFirstClick = true;
+    int discoveredMines = 0;
+    boolean shouldStop = false;
+    boolean isWin = false;
+
+    boolean isSaved = false;
+
+    /**
+     * Marks the time the game started.
+     */
+    long startTime;
+    /**
+     * Marks the time the game stopped. If the game is not stopped, marks the current time (every 200ms).
+     */
+    long stopTime;
+
+    /**
+     * <p>Updates <i>stopTime</i> and informative labels every 200ms.</p>
+     * <p>If <i>shouldStop</i> becomes true, the loop returns.</p>
+     */
+    Thread thread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            //todo rewrite this implement to fit the AI against mode
+            startTime = System.currentTimeMillis();
+            while (true) {
+//                print("while");
+                stopTime = System.currentTimeMillis();
+                Platform.runLater(() -> updateInformativeLabels());
+                if (shouldStop) {
+                    music.stop();
+                    try {
+                        if (isWin) {
+                            Sound.win();
+                        } else {
+                            Sound.gameOver();
+                        }
+                    } catch (Exception ignored) {
+                    }
+                    return;
+                }
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException ignored) {
+                }
+            }
+        }
+    });
+
+    //endregion
+    public AgainstAIController(int rows, int columns, int mines) throws IOException {
+        super(rows, columns, mines);
+    }
+
+    @Override
+    void clickedOnLabel(MouseClickType type, int row, int column) {
+        if (shouldStop) {
+            return;
+        }
+    }
+
+    @Override
+    void markGridLabel(int row, int column, LabelType type) {
+
+    }
+
+    @Override
+    void updateInformativeLabels() {
+
+    }
+
+    @Override
+    void closeStage() {
+
+    }
+}
--- src/app/Minefield/Robot.java
+++ src/app/Minefield/Robot.java
@@ -1,15 +1,59 @@
 package app.Minefield;
 
+import java.util.Random;
+
 import static app.PublicDefinitions.*;
 
 public class Robot {
-    public enum Difficulty {EASY, MEDIUM, HARD;}
 
-    private MinefieldType[][] clonedMinefield;
-    private Difficulty difficulty;
+    Random random = new Random();
 
-    public Robot(MinefieldType[][] minefieldTypes, Difficulty difficulty) {
-        clonedMinefield = minefieldTypes.clone();
-        this.difficulty = difficulty;
+    public void clickRandomly(AgainstAIController againstAIController) {
+        int i = random.nextInt(againstAIController.minefield.length);
+        int j = random.nextInt(againstAIController.minefield[0].length);
+        if (againstAIController.manipulatedMinefield[i][j] != LabelType.NOT_CLICKED) {
+            clickRandomly(againstAIController);
+        } else {
+            int temp = random.nextInt(4);
+            //Flag the spot with a percentage of 25%.
+            if (temp == 0) {
+                againstAIController.clickedOnLabel(MouseClickType.SECONDARY, i, j);
+            } else {
+                againstAIController.clickedOnLabel(MouseClickType.PRIMARY, i, j);
+            }
+        }
+    }
+
+    public void clickLikeAProfessor(AgainstAIController againstAIController) {
+        int i = random.nextInt(againstAIController.minefield.length);
+        int j = random.nextInt(againstAIController.minefield[0].length);
+        if (againstAIController.manipulatedMinefield[i][j] != LabelType.NOT_CLICKED) {
+            clickLikeAProfessor(againstAIController);
+        } else {
+            if (againstAIController.minefield[i][j] == MinefieldType.MINE) {
+                againstAIController.clickedOnLabel(MouseClickType.SECONDARY, i, j);
+            } else {
+                againstAIController.clickedOnLabel(MouseClickType.PRIMARY, i, j);
+            }
+        }
+    }
+
+    public void clickLikeAAmateur(AgainstAIController againstAIController) {
+        boolean probability = random.nextInt(3) == 0;
+        int i = random.nextInt(againstAIController.minefield.length);
+        int j = random.nextInt(againstAIController.minefield[0].length);
+        if (againstAIController.manipulatedMinefield[i][j] != LabelType.NOT_CLICKED) {
+            clickLikeAAmateur(againstAIController);
+        } else {
+            if (againstAIController.minefield[i][j] == MinefieldType.MINE) {
+                if (probability) {
+                    againstAIController.clickedOnLabel(MouseClickType.PRIMARY, i, j);
+                } else {
+                    againstAIController.clickedOnLabel(MouseClickType.SECONDARY, i, j);
+                }
+            } else {
+                againstAIController.clickedOnLabel(MouseClickType.PRIMARY, i, j);
+            }
+        }
     }
 }
