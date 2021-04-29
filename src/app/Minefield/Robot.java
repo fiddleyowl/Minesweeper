3abab5d3839f5d91b4d02aa61238ebd6cacbefde
FallenLucifer5 <82928147+FallenLucifer5@users.noreply.github.com>
2021-04-29 20:27:31 +0800

    Robot.

--- src/app/Minefield/Robot.java
+++ src/app/Minefield/Robot.java
@@ -6,6 +6,8 @@ import static app.PublicDefinitions.*;
 
 public class Robot {
 
+    //region Normal Robot
+
     Random random = new Random();
 
     public void clickRandomly(AgainstAIController againstAIController) {
@@ -56,4 +58,36 @@ public class Robot {
             }
         }
     }
+
+    //endregion
+
+    //region MSolver
+
+    class MSolver{
+
+        /**
+         *
+         * @param manipulatedMinefield
+         * @param i for row
+         * @param j for column
+         * @return
+         */
+        static int countUnopenedSquaresAround(LabelType[][] manipulatedMinefield,int i,int j){
+            int unopenedSquares = 0;
+
+            try { if (manipulatedMinefield[i-1][j-1] != LabelType.BOMBED && manipulatedMinefield[i-1][j-1] != LabelType.CLICKED){ unopenedSquares++; } }catch (Exception ignored){}
+            try { if (manipulatedMinefield[i-1][ j ] != LabelType.BOMBED && manipulatedMinefield[i-1][j-1] != LabelType.CLICKED){ unopenedSquares++; } }catch (Exception ignored){}
+            try { if (manipulatedMinefield[i-1][j+1] != LabelType.BOMBED && manipulatedMinefield[i-1][j-1] != LabelType.CLICKED){ unopenedSquares++; } }catch (Exception ignored){}
+            try { if (manipulatedMinefield[ i ][j+1] != LabelType.BOMBED && manipulatedMinefield[i-1][j-1] != LabelType.CLICKED){ unopenedSquares++; } }catch (Exception ignored){}
+            try { if (manipulatedMinefield[i+1][j+1] != LabelType.BOMBED && manipulatedMinefield[i-1][j-1] != LabelType.CLICKED){ unopenedSquares++; } }catch (Exception ignored){}
+            try { if (manipulatedMinefield[i+1][ j ] != LabelType.BOMBED && manipulatedMinefield[i-1][j-1] != LabelType.CLICKED){ unopenedSquares++; } }catch (Exception ignored){}
+            try { if (manipulatedMinefield[i+1][j-1] != LabelType.BOMBED && manipulatedMinefield[i-1][j-1] != LabelType.CLICKED){ unopenedSquares++; } }catch (Exception ignored){}
+            try { if (manipulatedMinefield[ i ][j-1] != LabelType.BOMBED && manipulatedMinefield[i-1][j-1] != LabelType.CLICKED){ unopenedSquares++; } }catch (Exception ignored){}
+
+            return unopenedSquares;
+        }
+
+    }
+
+    //endregion
 }
